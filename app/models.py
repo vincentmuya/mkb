7c0c8f61c25b19ef0d6c90f53bf21353d7b19902
vincentmuya <vincentmuya13@gmail.com>
2022-12-03 16:25:57 +0300

    created model landl

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Landlord(models.Model):
+    name = models.CharField(max_length=30)
+    PROPERTY = (
+        ('Flats/Apartments', 'Flats/Apartments'),
+        ('Bungalow', 'Bungalow'),
+    )
+    property = models.CharField(max_length=30, choices=PROPERTY)
+    tenant_unit = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
